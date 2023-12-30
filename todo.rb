8e56ccb2e7903eef2aef17bbc033996a20312dfb
Ping Honzay <phonzay1@gmail.com>
2023-12-30 01:33:57 +0000

    Optimized N+1 Query

--- todo.rb
+++ todo.rb
@@ -17,7 +17,11 @@ end
 
 helpers do
   def list_complete?(list)
-    todos_count(list) > 0 && todos_remaining_count(list) == 0
+    if list[:todos_count]
+      list[:todos_count] > 0 && list[:todos_remaining_count] == 0
+    else
+      todos_count(list) > 0 && todos_remaining_count(list) == 0
+    end
   end
 
   def list_class(list)
